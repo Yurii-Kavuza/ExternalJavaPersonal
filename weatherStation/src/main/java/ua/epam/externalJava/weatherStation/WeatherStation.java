e8ab50b688f29aa43df5d7ad2e20fc72747486dd
YuriiKavuza <kavuzaiurii@gmail.com>
2020-03-08 22:13:52 +0200

    Clear csv file

--- weatherStation/src/main/java/ua/epam/externalJava/weatherStation/WeatherStation.java
+++ weatherStation/src/main/java/ua/epam/externalJava/weatherStation/WeatherStation.java
@@ -3,7 +3,7 @@ package ua.epam.externalJava.weatherStation;
 import ua.epam.externalJava.weatherStation.observer.CurrentConditionsDisplay;
 import ua.epam.externalJava.weatherStation.observer.ForecastDisplay;
 import ua.epam.externalJava.weatherStation.observer.StatisticDisplay;
-import ua.epam.externalJava.weatherStation.parser.ParserXML;
+import ua.epam.externalJava.weatherStation.parser.ParserJSON;
 import ua.epam.externalJava.weatherStation.subject.WeatherCondition;
 import ua.epam.externalJava.weatherStation.subject.WeatherData;
 
@@ -12,11 +12,11 @@ import java.io.IOException;
 public class WeatherStation {
     public static void main(String[] args) throws IOException {
         WeatherData weatherData = new WeatherData();
-//        WeatherCondition weatherCondition = new WeatherCondition.Builder().
-//                temperature(ParserJSON.getTempCurrent()).
-//                humidity(ParserJSON.getHumidity()).
-//                pressure(ParserJSON.getPressure()).build();
-//        weatherData.setWeatherCondition(weatherCondition);
+        WeatherCondition weatherCondition = new WeatherCondition.Builder().
+                temperature(ParserJSON.getTempCurrent()).
+                humidity(ParserJSON.getHumidity()).
+                pressure(ParserJSON.getPressure()).build();
+        weatherData.setWeatherCondition(weatherCondition);
 
 //        WeatherCondition weatherCondition = new WeatherCondition.Builder().
 //                temperature(ParserJSON2.getTempCurrent()).
@@ -24,16 +24,17 @@ public class WeatherStation {
 //                pressure(ParserJSON2.getPressure()).build();
 //        weatherData.setWeatherCondition(weatherCondition);
 
-        WeatherCondition weatherCondition = new WeatherCondition.Builder().
-                temperature(ParserXML.getTempCurrent()).
-                humidity(ParserXML.getHumidity()).
-                pressure(ParserXML.getPressure()).build();
-        weatherData.setWeatherCondition(weatherCondition);
+//        WeatherCondition weatherCondition = new WeatherCondition.Builder().
+//                temperature(ParserXML.getTempCurrent()).
+//                humidity(ParserXML.getHumidity()).
+//                pressure(ParserXML.getPressure()).build();
+//        weatherData.setWeatherCondition(weatherCondition);
 
         CurrentConditionsDisplay currentConditionsDisplay =
                 new CurrentConditionsDisplay(weatherData);
         StatisticDisplay statisticDisplay = new StatisticDisplay(weatherData);
         ForecastDisplay forecastDisplay = new ForecastDisplay(weatherData);
+
 
         weatherData.setMeasurements(weatherCondition);
     }
